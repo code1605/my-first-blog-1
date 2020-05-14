bff041c165e8bd5fee50c346d9534089371cdce1
willhitman <giftwt9wt@gmail.com>
2020-05-14 15:32:35 +0200

    ...

--- blog/views.py
+++ blog/views.py
@@ -41,6 +41,3 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
-def logout(request):
-    logout(request)
